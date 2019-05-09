822f998b43ee57f8d43698f2659666e8b71b1819
idan agam <edanagam@yahoo.com>
2019-05-10 00:36:49 +0300

    adding watch for fields

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,30 @@
 <template>
+<mdbContainer>
   <section class="form-simple">
     <mdb-row class="justify-content-center mx-auto mt-5">
       <mdb-col md="5">
         <mdb-card>
-          <div class="header pt-3 grey lighten-2">
+          <div class="header pt-3 blue lighten-2">
             <mdb-row class="d-flex justify-content-center">
               <h3 class="deep-grey-text mt-3 mb-4 pb-1 mx-5">Log in</h3>
             </mdb-row>
           </div>
           <mdb-card-body class="mx-4 mt-4">
-            <mdb-input label="Your email" type="text"/>
-            <mdb-input label="Your password" type="password" containerClass="mb-0"/>
+            <mdb-input v-model="email" label="Your email" type="text"/>
+            <mdb-input v-model="password" label="Your password" type="password" containerClass="mb-0"/>
             <div class="text-center mb-4 mt-5">
-              <mdb-btn type="button" gradient="blue" rounded class="btn-block z-depth-1a">Sign in</mdb-btn>
+              <mdb-btn type="button" gradient="blue" rounded class="btn-block z-depth-1a" :disabled="!emailValidated || !passwordValidated">Sign in</mdb-btn>
             </div>
           </mdb-card-body>
         </mdb-card>
       </mdb-col>
     </mdb-row>
   </section>
+</mdbContainer>
 </template>
 
 <script>
-import { mdbRow, mdbCol, mdbCard, mdbCardBody, mdbInput, mdbBtn } from 'mdbvue'
+import { mdbRow, mdbCol, mdbCard, mdbCardBody, mdbInput, mdbBtn, mdbContainer } from 'mdbvue'
 export default {
   name: 'FormsPage',
   components: {
@@ -31,7 +33,24 @@ export default {
     mdbCard,
     mdbCardBody,
     mdbInput,
-    mdbBtn
+    mdbBtn,
+    mdbContainer
+  },
+  data () {
+    return {
+      email: null,
+      password: null,
+      emailValidated: false,
+      passwordValidated: false
+    }
+  },
+  watch: {
+    email (val) {
+      console.log(val)
+    },
+    password (val) {
+      console.log(val)
+    }
   }
 }
 </script>
