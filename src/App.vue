03e6dde2624a0aec1dfec0d6cbf2443947bd8a9e
idan agam <edanagam@yahoo.com>
2019-06-20 18:46:46 +0300

    Login bug fix

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
   mounted () {
     this.navbarVisible = this.$route.name !== 'login'
     if (this.navbarVisible) {
-      this.$socket.emit('logedin')
+      this.$root.$emit('logedIn')
     }
     this.$root.$on('logedIn', () => {
       this.navbarVisible = true
